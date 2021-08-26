47a63c051f22127c1aa4a27fc830e4dc1ccb7278
Frankess.Sus <71155079+lengoclong2k@users.noreply.github.com>
2021-08-26 21:43:04 +0700

    first commit

---
+++ djackets/src/views/SignUp.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="page-sign-up">
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title"> Sign Up </h1>
+                <form @submit.prevent="submitForm">
+                    <div class="field">
+                        <label>Tên đăng nhập </label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="username">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Mật khẩu </label>
+                        <div class="control">
+                            <input type="password" class="input" v-model="password">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Nhập lại mật khẩu </label>
+                        <div class="control">
+                            <input type="password" class="input" v-model="password2">
+                        </div>
+                    </div>
+
+                    <div class="notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                    </div>
+
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-dark">Đăng kí</button>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    Or <router-link to="/log-in">Ấn vào đây</router-link> nếu bạn đã có tài khoản !
+                </form>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import {toast} from 'bulma-toast'
+export default {
+    name: 'SignUp',
+    data(){
+        return{
+            username: '',
+            password: '',
+            password2: '',
+            errors:[]
+        }
+    },
+    mounted(){
+        document.title = 'Sign Up | Djackets'
+    },
+    methods:{
+        submitForm(){
+            this.errors =[]
+
+            if(this.username === ''){
+                this.errors.push('Yêu cầu nhập tên đăng nhập')
+            }
+            if(this.password ===''){
+                this.errors.push('Yêu cầu nhập mật khẩu')
+            }
+            if(this.password !== this.password2){
+                    this.errors.push('Mật khẩu không khớp. Vui lòng nhập lại mật khẩu ')
+            }
+            if(!this.errors.length){
+                const formData = {
+                    username: this.username,
+                    password: this.password
+                }
+
+                axios
+                    .post("/api/v1/users/",formData)
+                    .then(response =>{
+                        toast({
+                            message: 'Account created, please log in!',
+                            type: 'is-success',
+                            dismissible: true,
+                            pauseOnHover: true,
+                            duration: 2000,
+                            position: 'bottom-right',
+                        })
+
+                        this.$router.push('/log-in')
+                    })
+                    .catch(error =>{
+                        if(error.response){
+                            for(const property in error.response.data){
+                                this.errors.push(`${property}: ${error.response.data[property]}`)
+                            }
+                            console.log(JSON.stringify(error.response.data))
+                        }
+                        else if(error.message){
+                            this.errors.push('Something went wrong. Please try again')
+
+                            console.log(JSON.stringify(error))
+                        }
+                    })
+            }
+        }
+    }
+}
+</script>
---
+++ djackets/src/views/Success.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="page-success">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <p class="title"> Đơn hàng của bạn đang được xử lí </p>
+                <h1 class="title"> Cảm ơn bạn đã dùng dịch vụ bên chúng tôi.Hẹn gặp lại</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Success',
+    mounted(){
+        document.title = 'Success | FrankesSus'
+    },
+}
+</script>
